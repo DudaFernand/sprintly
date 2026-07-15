a874200dcab10289ef42bc0068def58280b6181a
Duda Fernandes <134013170+DudaFernand@users.noreply.github.com>
2026-07-15 10:27:26 -0300

    chore: setup inicial do projeto Sprintly com Spring Boot e Postgres

---
+++ src/test/java/com/mariafernandes/sprintly/SprintlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariafernandes.sprintly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
